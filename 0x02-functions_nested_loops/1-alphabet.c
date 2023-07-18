cbc83e8be6aabb665f29c545b38a6c7faf6954ce
Havvah <havavicky@gmail.com>
2023-07-18 16:26:33 -0700

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_alphabet - prints alphabet in lowercase
+  * Return: void
+  * prints alphabet on lower case
+  */
+void print_alphabet(void)
+{
+	int c;
+	c = 97;
+	while (c <= 122)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
