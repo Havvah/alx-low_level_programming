1645d5601412d105d67d87723d9fc56f5c490b8c
Havvah <havavicky@gmail.com>
2023-07-20 15:31:29 -0700

    Functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print 0 to 9 excluding 2 and 4
- * Return: void
+ * print_most_numbers - print num except 2 and 4
+ *
+ * return: void
  */
 
 void print_most_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = '0' ; i < 10 ; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (i != 2 && != 4)
-			_putchar(i + '0');
-
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
-}	_putchar('\n');
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * more_numbers - print numbers 0 to 14
- * Return: void
+ * more_numbers - print more numbers
  */
 
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0 ; j <= 14 ; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}
-}	_putchar('\n');
+}
