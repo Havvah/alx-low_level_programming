c4fbf12a8f18fe866f5dbb2a1ff2e8c7c5653cb5
Havvah <havavicky@gmail.com>
2023-07-24 14:23:20 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,46 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * _atoi -  function that convert a string to an integer
+ * @s: string pointer
+ * Return: 0 un success
  */
 
 int _atoi(char *s)
+
 {
-	int i, d, n, len, f, digit;
+        int x = 0;
+        unsigned int n = 0;
+        int min = 1;
+        int isi = 0;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && f == 0)
-	{
-		if (s[i] == '_')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-
-	}
-	if (f == 0)
-		return (0);
-
-	return (n);
+        while (s[x])
+        {
+                if (s[x] == 45)
+                {
+                        min *= -1;
+                }
+                while (s[x] >= 48 && s[x] <= 57)
+                {
+                        isi = 1;
+                        n = (n * 10) + (s[x] - '0');
+                        x++;
+                }
+                if (isi == 1)
+                {
+                        break;
+                }
+                x++;
+        }
+        n *= min;
+        return (n);
 }
