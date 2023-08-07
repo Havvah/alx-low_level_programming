3b82cfd38eea3314af47075ea3872b5ea754fdf0
Havvah <havavicky@gmail.com>
2023-08-07 11:25:11 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,42 +1,42 @@
 #include "main.h"
 
 /**
- * argstostr - convert the parameter passed to the program to string
- * @ac: the argument count
- * @av: the argument vector
- * Return: Pointer to a new string containing concatenated arguments
- */
+* argstostr - convert the parameter passed to the program to string
+* @ac: the argument count
+* @av: the argument vector
+* Return: Pointer to a new string containing concatenated arguments
+*/
 
 char *argstostr(int ac, char **av)
 {
-	int ch = 0, i = 0, j = 0, k = 0;
-	char *s;
+int ch = 0, i = 0, j = 0, k = 0;
+char *s;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			ch++;
-		}
-	}
-	s = malloc(sizeof(char) * (ch + 1));
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+ch++;
+}
+}
+s = malloc(sizeof(char) * (ch + 1));
 
-	if (s == NULL)
-		return (NULL);
+if (s == NULL)
+return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			s[k] = av[i][j];
-			k++;
-		}
-		s[k] = '\n';
-		k++;
-	}
-	s[k] = '\0';
-	return (s);
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+s[k] = av[i][j];
+k++;
+}
+s[k] = '\n';
+k++;
+}
+s[k] = '\0';
+return (s);
 }
