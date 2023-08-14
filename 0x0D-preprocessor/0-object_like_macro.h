af5f2d61c329ad880a935dc22637684d3cfc9243
Havvah <havavicky@gmail.com>
2023-08-14 05:10:34 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef _OBJECT_LIKE_MACRO_H_
+#define _OBJECT_LIKE_MACRO_H_
+
+/* Defines a macro named SIZE as an abbreviation for the token 1024 */
+#define SIZE 1024
+
+#endif /* _OBJECT_LIKE_MACRO_H_ */
+
