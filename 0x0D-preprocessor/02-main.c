9c14c9cc3fd98b76e921bb9ced96eb8cdb1d0346
Havvah <havavicky@gmail.com>
2023-08-14 05:28:53 -0700

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the file it was compiled from.
+ * The filename is dynamically inserted into the output message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("This program was compiled from the file: %s\n", __FILE__);
+	return (0);
+}
+
